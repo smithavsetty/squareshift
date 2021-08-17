0c03e0fd984c659127e3392567420b6b3686577d
smithavsetty <89000150+smithavsetty@users.noreply.github.com>
2021-08-18 00:14:28 +0530

    Update BaseMethods.java
    Removed Waits

--- src/main/java/pageobjects/BaseMethods.java
+++ src/main/java/pageobjects/BaseMethods.java
@@ -5,6 +5,7 @@ import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
@@ -22,41 +23,46 @@ public class BaseMethods {
 	public void scrollToEnd() {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollTo(0, document.body.scrollHeight)");
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	}
+
+	public void scrollToTop() {
+		JavascriptExecutor js = (JavascriptExecutor) driver;
+		js.executeScript("window.scrollTo(0, 0)");
 	}
 
 	public void scrollUp(String size) {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0," + size + ")");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 	public void scrollDown(String size) {
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("window.scrollBy(0," + size + ")");
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 	}
 
 	public void scrollToView(String e) {
 		By locator = ObjectLocators.getBySelector(e);
+		while (!isElementPresent(locator)) {
+			scrollUp("-500");
+			driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		}
 		WebElement element = driver.findElement(locator);
 		JavascriptExecutor js = (JavascriptExecutor) driver;
 		js.executeScript("arguments[0].scrollIntoView();", element);
-		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+		// wait(elementToBePresent(locator)); Fluent wait
 	}
 
 	public void click(String e) {
 		By locator = ObjectLocators.getBySelector(e);
 		WebElement element = driver.findElement(locator);
 		element.click();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
 
 	public void clickElements(String e) {
 		By locator = ObjectLocators.getBySelector(e);
 		List<WebElement> element = driver.findElements(locator);
 		element.get(1).click();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 	}
 
 	public void getUrl(String url) {
@@ -69,4 +75,34 @@ public class BaseMethods {
 		return element.getText();
 	}
 
+	public boolean isElementPresent(By by) {
+		try {
+			WebElement element = driver.findElement(by);
+			if (element != null) {
+				return true;
+			}
+			return false;
+		} catch (NoSuchElementException e) {
+			return false;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	/**
+	 * Added fluet waits , but still we get ElementClickInterceptedException
+	 * exception hence couldn't avoid adding one dynamic wait
+	 */
+	/*
+	 * public static ExpectedCondition<WebElement> elementToBePresent(By by) {
+	 * return ExpectedConditions.presenceOfElementLocated(by); }
+	 * 
+	 * public <U> U wait(ExpectedCondition<U> condition) {
+	 * 
+	 * FluentWait<WebDriver> wait = new
+	 * FluentWait<WebDriver>(this.driver).ignoring(RuntimeException.class)
+	 * .withTimeout(30, TimeUnit.SECONDS).pollingEvery(500,
+	 * TimeUnit.MILLISECONDS); try { return wait.until(condition); } catch
+	 * (TimeoutException err) { return null; } }
+	 */
 }
